9e6effeb98932d273cd99a2f35deee8d536dd88e
Daniel\ Herbison <daniel.herbison@us.idemia.com>
2021-03-29 17:34:22 +0100

    main

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -63,7 +63,7 @@ class PrivateIngredientsAPITests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
-        self.assertEqual(res.data[1]['name'], ingredient.name)
+        self.assertEqual(res.data[0]['name'], ingredient.name)
 
     def test_create_ingredient_successful(self):
         """Test creating a new ingredient"""
